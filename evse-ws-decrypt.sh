19d97d7c7533565a8b24dc9bb486094f82885f5a
Tim Aerts <aardbeiplantje@gmail.com>
2026-07-16 11:24:28 +0200

    evse-ws-decrypt.sh: add proper usage on wrong parameters at commandline

--- evse-ws-decrypt.sh
+++ evse-ws-decrypt.sh
@@ -3,8 +3,8 @@ sfn=$1
 shift
 kfn=$1
 shift
-[[ ! -f "$sfn" ]] && exit 1
-[[ ! -f "$kfn" ]] && exit 1
+[[ ! -f "$sfn" ]] && { echo "usage: $BASH_SOURCE <tcpdumpfile> <keyfile>"; exit 1; }
+[[ ! -f "$kfn" ]] && { echo "usage: $BASH_SOURCE <tcpdumpfile> <keyfile>"; exit 1; }
 where=$(readlink -f ${BASH_SOURCE})
 websocket_lua_script_file=${where%/*}/ws.lua
 tshark -r "$sfn" -2 -R \
